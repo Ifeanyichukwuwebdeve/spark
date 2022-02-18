da49740b18d55fcb1073c00ea273e6695e6f7812
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-02-18 19:07:27 +0100

    Started some work on the transactions section

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,111 @@
+import React, { useContext } from 'react'
+
+import { TransactionContext } from '../context/TransactionContext';
+import { shortenAddress } from '../utils/shortAddress';
+
+const TransactionsCard = ({ addressTo, addressFrom, timeStamp, message, keyword, amount, url }) => {
+  return (
+    <div className='bg-[#181918] m-4 flex flex-1
+    2xl:min-w-[450px]
+    2xl:max-w-[500px]
+    sm:min-w-[270px]
+    sm:min-w-[300px]
+    flex-col p-3 rounded-md hover:shadow-2xl'>
+      <div className='flex flex-col items-center w-full mt-3'>
+        <div className='w-full mb-6 p-2'>
+          <a href={`https://rinkeby.etherscan.io/address/${addressFrom}`} target='_blank' rel='noopener noreferrer'>
+            <p className='text-white text-base'>From: {shortenAddress(addressFrom)}</p>
+          </a>
+          <a href={`https://rinkeby.etherscan.io/address/${addressTo}`} target='_blank' rel='noopener noreferrer'>
+            <p className='text-white text-base'>To: {shortenAddress(addressTo)}</p>
+          </a>
+
+          <p className='text-white text-base'>Amount: {amount} ETH</p>
+          <p className='text-white text-base'>Message: {message}</p>
+
+          <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'>
+            <p className='text-[#37c7da] font-bold'>{timeStamp}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext)
+  const dummyContent = [
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    },
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    },
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    },
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    },
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    },
+    {
+      addressTo: '0x02dsd',
+      addressFrom: '0xw434324fsdgtydrr453r',
+      timeStamp: 432,
+      message: 'Tst',
+      keyword: 'Test',
+      amount: 20,
+      url: 'https://greatadams.com/pis'
+    }
+]
+  
   return(
-    <h1>Transactions</h1>
+    <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+      <div className='flex flex-col md:p-12 py-12 px-4'>
+        {currentAccount ? (
+          <h3 className='text-white text-3xl text-center my-2'>Lastest Transactions</h3>
+        ) : (
+          <h3 className='text-white text-3xl text-center my-2'>Connect your account to see latest transactions</h3>
+        )}
+        <div className='flex flex-wrap justify-center items-center mt-10'>
+          {dummyContent.reverse().map((transaction, i) => (
+            <TransactionsCard key={i} {...transaction}/>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
