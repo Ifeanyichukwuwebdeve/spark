297d6a18e27f744a5fd050fa71199b51fe81f9ed
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-02-18 02:26:40 +0100

    Deployed the contract to rinkeby testnet

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,21 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/1JjWJZsSBD2eD7_s1LBJG5xtSKhBsnaw',
+      accounts: [ '92f67767b1a7bb3f8c5c9881a7c4993ca6c9aae2850b43a87fa9ad26d3079e71' ]
+    }
+  }
 };
